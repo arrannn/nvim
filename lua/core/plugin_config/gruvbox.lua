0390279cdac2da50fa2585f9818a773542a6db3c
Arran <arranmt@gmail.com>
2023-01-22 23:01:28 +0000

    lua

---
+++ lua/core/plugin_config/gruvbox.lua
@@ -0,0 +1,8 @@
+
+vim.o.termguicolors = true
+vim.cmd [[ colorscheme gruvbox ]]
+
+vim.cmd [[ highlight Normal         ctermbg=NONE guibg=NONE ]]
+vim.cmd [[ highlight LineNr         ctermbg=NONE guibg=NONE ]]
+vim.cmd [[ highlight SignColumn     ctermbg=NONE guibg=NONE ]]
+vim.cmd [[ highlight EndOfBuffer    ctermbg=NONE guibg=NONE ]]
